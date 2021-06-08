c1387a48390e62a04a060ddd0495e7ca3fff410d
Yang Jim <jim.yang@ubtrobot.com>
2021-06-07 23:29:56 -0400

    init

---
+++ website/scripts.js
@@ -0,0 +1,107 @@
+{/* <p>This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+This is about othe thing
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br> */}
